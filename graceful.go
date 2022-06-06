4787c7681d5010599810c6357eb221ef3954b755
BostinWang <bostin@BostinWangdeMacBook-Pro.local>
2022-06-06 11:26:37 +0800

    U

--- graceful.go
+++ graceful.go
@@ -14,7 +14,14 @@ type Priority int
 const (
 	PriorityLevel0  Priority = 0
 	PriorityLevel10 Priority = 10
-	PriorityLevel99 Priority = 99
+	PriorityLevel20 Priority = 20
+	PriorityLevel30 Priority = 30
+	PriorityLevel40 Priority = 40
+	PriorityLevel50 Priority = 50
+	PriorityLevel60 Priority = 60
+	PriorityLevel70 Priority = 70
+	PriorityLevel80 Priority = 80
+	PriorityLevel90 Priority = 90
 )
 
 type Graceful interface {
@@ -26,15 +33,16 @@ type Graceful interface {
 // Graceful
 // 平滑关闭
 type graceful struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-	wg     *sync.WaitGroup
-	lock   sync.Mutex
-	jobs   map[Priority][]func()
+	ctx     context.Context
+	cancel  context.CancelFunc
+	wg      *sync.WaitGroup
+	lock    sync.Mutex
+	jobs    map[Priority][]func()
+	signals []os.Signal
 }
 
 func (g *graceful) Priorities() []Priority {
-	return []Priority{PriorityLevel0, PriorityLevel10, PriorityLevel99}
+	return []Priority{PriorityLevel0, PriorityLevel10, PriorityLevel20, PriorityLevel30, PriorityLevel40, PriorityLevel50, PriorityLevel60, PriorityLevel70, PriorityLevel80, PriorityLevel90}
 }
 
 // Register
@@ -53,7 +61,7 @@ func (g *graceful) Register(p Priority, f func()) {
 // 信号监听
 func (g *graceful) Listen() {
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sig, g.signals...)
 
 	go func() {
 		<-sig
@@ -77,13 +85,17 @@ func (g *graceful) Listen() {
 	g.wg.Wait()
 }
 
-func NewGraceful(ctx context.Context, cancel context.CancelFunc) Graceful {
+func NewGraceful(ctx context.Context, cancel context.CancelFunc, signals ...os.Signal) Graceful {
+	if len(signals) == 0 || signals == nil {
+		signals = []os.Signal{syscall.SIGTERM, syscall.SIGINT}
+	}
 	shutdown := &graceful{
-		ctx:    ctx,
-		cancel: cancel,
-		wg:     &sync.WaitGroup{},
-		lock:   sync.Mutex{},
-		jobs:   make(map[Priority][]func()),
+		ctx:     ctx,
+		cancel:  cancel,
+		wg:      &sync.WaitGroup{},
+		lock:    sync.Mutex{},
+		jobs:    make(map[Priority][]func()),
+		signals: signals,
 	}
 	// 确保所有的回调都被执行
 	shutdown.wg.Add(1)
